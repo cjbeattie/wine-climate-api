df167796c703aef270f240888879071dcbbc70a8
cjbeattie <courtney.j.beattie+1@gmail.com>
2025-03-06 13:29:53 +1000

    add wine region table

--- wine_climate/climate_api/models.py
+++ wine_climate/climate_api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class WineRegion(models.Model):
+    name = models.CharField(max_length=255)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)  # Store latitude as a decimal
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)  # Store longitude as a decimal
+
+    def __str__(self):
+        return self.name
